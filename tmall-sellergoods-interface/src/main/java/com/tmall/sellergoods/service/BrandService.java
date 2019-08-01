33a59c87a7c1892ac0f8af5259af03aaed2a49e9
wenlo <wenlongzhou@aliyun.com>
2019-08-01 19:56:10 +0800

    modify

--- tmall-sellergoods-interface/src/main/java/com/tmall/sellergoods/service/BrandService.java
+++ tmall-sellergoods-interface/src/main/java/com/tmall/sellergoods/service/BrandService.java
@@ -1,6 +1,7 @@
 package com.tmall.sellergoods.service;
 
 import com.tmall.pojo.Brand;
+import entity.PageResult;
 
 import java.util.List;
 
@@ -8,5 +9,19 @@ import java.util.List;
 public interface BrandService {
 
     public List<Brand> findAll();
+
+    public PageResult findPage(int pageNum, int pageSize);
+
+    public void add(Brand brand);
+
+    public Brand findOne(int id);
+
+    public void update(Brand brand);
+
+    public void delete(int[] ids);
+
+    public void delete(int id);
+
+    public PageResult findPage(Brand brand, int pageNum, int pageSize);
 
 }
