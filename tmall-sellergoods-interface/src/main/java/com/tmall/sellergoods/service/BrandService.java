d3b295ed8e588a7dbbb0c8e19c1e3da299d8ea52
wenlo <wenlongzhou@aliyun.com>
2019-08-01 08:17:25 +0800

    first commit

---
+++ tmall-sellergoods-interface/src/main/java/com/tmall/sellergoods/service/BrandService.java
@@ -0,0 +1,12 @@
+package com.tmall.sellergoods.service;
+
+import com.tmall.pojo.Brand;
+
+import java.util.List;
+
+//品牌接口
+public interface BrandService {
+
+    public List<Brand> findAll();
+
+}
