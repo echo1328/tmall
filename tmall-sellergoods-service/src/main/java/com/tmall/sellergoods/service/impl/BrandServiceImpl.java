96dcd725fe7202a286959b3f966f4dd936cc02c1
wenlo <wenlongzhou@aliyun.com>
2019-08-01 19:57:05 +0800

    modify

--- tmall-sellergoods-service/src/main/java/com/tmall/sellergoods/service/impl/BrandServiceImpl.java
+++ tmall-sellergoods-service/src/main/java/com/tmall/sellergoods/service/impl/BrandServiceImpl.java
@@ -1,9 +1,13 @@
 package com.tmall.sellergoods.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.tmall.mapper.BrandMapper;
 import com.tmall.pojo.Brand;
+import com.tmall.pojo.BrandExample;
 import com.tmall.sellergoods.service.BrandService;
+import entity.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -24,6 +28,58 @@ public class BrandServiceImpl implements BrandService {
     @Override
     public List<Brand> findAll() {
         return brandMapper.selectByExample(null);
+    }
+
+    @Override
+    public PageResult findPage(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum,pageSize);
+        Page<Brand> page = (Page<Brand>) brandMapper.selectByExample(null);
+        return new PageResult(page.getTotal(),page.getResult());
+    }
+
+    @Override
+    public void add(Brand brand) {
+        brandMapper.insert(brand);
+    }
+
+    @Override
+    public Brand findOne(int id) {
+        return brandMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(Brand brand) {
+        brandMapper.updateByPrimaryKey(brand);
+    }
+
+    @Override
+    public void delete(int[] ids) {
+        for(int id : ids) {
+            brandMapper.deleteByPrimaryKey(id);
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        brandMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public PageResult findPage(Brand brand, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        BrandExample example=new BrandExample();
+        BrandExample.Criteria criteria = example.createCriteria();
+        if(brand!=null){
+            if(brand.getName()!=null && brand.getName().length()>0){
+                criteria.andNameLike("%"+brand.getName()+"%");
+            }
+            if(brand.getFirst_char()!=null && brand.getFirst_char().length()>0){
+                criteria.andFirst_charEqualTo(brand.getFirst_char());
+            }
+        }
+        Page<Brand> page = (Page<Brand>) brandMapper.selectByExample(example);
+        System.out.println(page.getTotal());
+        return new PageResult(page.getTotal(),page.getResult());
     }
 
 }
