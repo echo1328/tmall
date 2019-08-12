cf1985f7bf90b95d9a091416a886bd0358717851
wenlo <wenlongzhou@aliyun.com>
2019-08-12 09:20:06 +0800

    commit

--- tmall-elasticsearch/src/main/java/com/tmall/search/repositories/ArticleRepository.java
+++ tmall-elasticsearch/src/main/java/com/tmall/search/repositories/ArticleRepository.java
@@ -3,9 +3,11 @@ package com.tmall.search.repositories;
 import com.tmall.search.entity.Article;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ArticleRepository extends ElasticsearchRepository<Article, Integer> {
 
     List<Article> findByTitle(String title);
